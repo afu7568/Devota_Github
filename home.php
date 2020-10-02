0c68dc8451bc80f0a4bb7c09bf1ff8feca780955
Alan Fu <afu7568@stacmail.net>
2020-10-02 14:52:38 +1300

    logo change

--- home.php
+++ home.php
@@ -1,19 +1,16 @@
 <!-- The Home Page. This is the default page displayed on the website. Shows the website's branding,
  slogan, and gives links to the login and sign up page.-->
 
-<div class='row'>
+<div class='row py-5'>
   <!-- The websites logo, name, and slogan -->
-  <div class='col-12 mt-5 text-center'>
-    <img src='img/log.png' height='100%' width='250px'>
+  <div class='col-12 mt-5 pt-5 text-center'>
+    <img src='img/full_logo.png' height='100%' width='400px'>
     <br>
   </div>
-  <div class='col-1 col-lg-2 col-xl-3'></div>
-  <div class='col-10 col-lg-8 col-xl-6 text-center'>
+  <div class='col-md-2 col-lg-3'></div>
+  <div class='col-md-8 col-lg-6 text-center'>
     <br>
-    <h3 >Technology Solutions</h2>
-    <p >
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <h2>Bringing back the freedom and independence everyone loves</h2>
   </div>
-  <div class='col-1 col-lg-2 col-xl-3'></div>
+  <div class='col-md-2 col-lg-3 mt-5'></div>
 </div>
